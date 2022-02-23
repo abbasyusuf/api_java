c52ab5c8180e0248bbbb697b561a60aa7dd21776
Abbas Yusuf <abbas.ws@gmail.com>
2022-02-23 16:14:52 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const api ='https://www.breakingbadapi.com/api/characters'
+
+async function get(){
+    const response = await fetch(api)
+    const data = await response.json()
+    console.log(data)
+
+    document.querySelector('#content h1').innerHTML = data[0].name
+    document.querySelector('#content img').src = data[0].img
+    document.querySelector('#content p').innerHTML = data[0].nickname
+
+
+    // Example of a list
+    document.querySelector('#actor select').innerHTML = data.map(abbas => `<option>${abbas.name}</option>`)
+}
+
+get()
